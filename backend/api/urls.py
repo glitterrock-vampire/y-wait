44d75388939758b3facf66764a4a4b4cf0e81d06
glitterrock-vampire <waltersdelano97@gmail.com>
2025-03-14 12:26:28 -0500

    Add empty frontend folder

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, QueueViewSet
+from .views import UserViewSet, QueueViewSet, predict_wait_time
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -8,4 +8,6 @@ router.register(r'queues', QueueViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('queues/<int:queue_id>/predict/', predict_wait_time, name='predict-wait-time'),
+
 ]
